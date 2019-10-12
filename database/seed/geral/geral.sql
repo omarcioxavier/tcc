4a39aad9aeafd05e6579af2916667915c26da5ca
Márcio Júnior <junior_xavier60@hotmail.com>
2019-10-12 10:13:35 -0300

    Pedidos detalhes (2)

--- database/seed/geral/geral.sql
+++ database/seed/geral/geral.sql
@@ -66,8 +66,8 @@ INSERT INTO produto (descricao, precoUnitario, ativo, produtoCategoriaID) VALUES
 INSERT INTO estabelecimentoProduto (estabelecimentoID, produtoID) VALUES
 	(1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8),	(1,9), (1,10), (1,11), (2,12), (2,13), (2,14), (2,15), (2,16), (2,8), (2,10), (2,11);
 
-INSERT INTO pedido (dataPedido, valorPedido, entrega, clienteID, estabelecimentoID) VALUES
-	(GETDATE(), 47, 0, 1, 1);
+INSERT INTO pedido (dataPedido, valorPedido, quantidadeTotal, entrega, clienteID, estabelecimentoID) VALUES
+	(GETDATE(), 47, 2, 0, 1, 1);
 
-INSERT INTO produtoPedido (pedidoID, produtoID) VALUES 
-	(1,1), (1,8);
+INSERT INTO produtoPedido (pedidoID, produtoID, quantidade) VALUES 
+	(1,1,1), (1,8,1);
