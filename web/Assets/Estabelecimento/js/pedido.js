99374dbefde4bcd91f94d16d65bc9e0e92077b65
Márcio Júnior <junior_xavier60@hotmail.com>
2019-11-15 18:35:25 -0300

    Validar Endereço google maps (1)

---
+++ web/Assets/Estabelecimento/js/pedido.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () { });
+
+// Carrega o endereço no maps de acordo com as coordenadas
+function loadGMaps() {
+    const latitude = Number($("#hddLatitude").val().replace(",", "."));
+    const longitude = Number($("#hddLongitude").val().replace(",", "."));
+
+    var mapProp = {
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: 15,
+    };
+
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var marker = new google.maps.Marker({ position: mapProp.center });
+    marker.setMap(map);
+}
+
+function ValidarEndereco() {
+    var endereco = "";
+
+    var urlGMapsApi = "https://maps.googleapis.com/maps/api/geocode/json?address=" + endereco + "&key=AIzaSyDoOBBqHuu3tiGZ4v46MGMN4c5J10xbntk";
+
+    $.get(urlGMapsApi, function (data, status) {
+        console.log(status);
+        console.log(data);
+    });
+}
