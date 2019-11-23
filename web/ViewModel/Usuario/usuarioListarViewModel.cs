3c3f9feb5c397cdcb8d850140b0301640e60d1b9
Márcio Júnior <junior_xavier60@hotmail.com>
2019-11-23 12:32:01 -0300

    Pesquisa usuário

---
+++ web/ViewModel/Usuario/usuarioListarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using web.Models.Usuario;
+
+namespace web.ViewModel.Usuario
+{
+    public class usuarioListarViewModel
+    {
+        public IEnumerable<usuario> usuarios { get; set; }
+
+        public string login { get; set; }
+
+        public bool ativo { get; set; }
+    }
+}
